06c8f41b6a940e165323816f79db102fd05523ef
steffen.stenersen@gmail.com <steffen.stenersen@gmail.com@95fc8098-fdfb-d47c-ab93-935c36e4efd7>
2011-03-02 13:54:41 +0000

    Added all the base classes.

---
+++ trunk/League of Leprechauns/League of Leprechauns/classes/GameState.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace League_of_Leprechauns
+{
+    enum GameState
+    {
+        PLAYING,
+        PAUSED,
+        DEAD,
+        GAME_OVER,
+        MENU
+    }
+}
