ba78513c9eb3320ca79226aa2c9229e6bd31b03a
steffen.stenersen@gmail.com <steffen.stenersen@gmail.com@95fc8098-fdfb-d47c-ab93-935c36e4efd7>
2011-03-02 13:54:41 +0000

    Added all the base classes.

---
+++ League of Leprechauns/League of Leprechauns/classes/Actors/Actor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace League_of_Leprechauns
+{
+    class Actor
+    {
+        static List<Actor> Actors;
+        private Texture2D texture;
+        private Vector2 position;
+
+        static Actor()
+        {
+            Actors = new List<Actor>();
+        }
+
+        public Actor()
+        {
+            Actors.Add(this);
+        }
+
+        public void Update(GameTime gameTime)
+        {
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+
+        }
+    }
+}
