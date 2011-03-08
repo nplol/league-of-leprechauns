e39190836e5e739749bd6fbee17fbc8df8742feb
kengroenn@gmail.com <kengroenn@gmail.com@95fc8098-fdfb-d47c-ab93-935c36e4efd7>
2011-03-08 15:48:17 +0000

    Generic Version of Actor.cs Implemented

--- trunk/League of Leprechauns/League of Leprechauns/classes/Actors/Actor.cs
+++ trunk/League of Leprechauns/League of Leprechauns/classes/Actors/Actor.cs
@@ -10,9 +10,48 @@ namespace League_of_Leprechauns
     class Actor
     {
         static List<Actor> Actors;
+        private SpriteEffects spriteEffect;
         private Texture2D texture;
-        private Vector2 position;
 
+        public float Depth
+        {
+            set;
+            get;
+        }
+
+        public Vector2 Origin
+        {
+            get { return new Vector2(texture.Width / 2, texture.Height / 2); }
+
+        }
+
+        public Vector2 Position
+        {
+            get;
+            set;
+        }
+
+        public float Rotation
+        {
+            get;
+            set;
+        }
+
+        public float Scale
+        {
+            get;
+            set;
+        }
+
+        public void flipHorizontally()
+        {
+            if (spriteEffect == SpriteEffects.FlipHorizontally)
+                spriteEffect = SpriteEffects.None;
+            else
+                spriteEffect = SpriteEffects.FlipHorizontally;
+        }
+
+       
         static Actor()
         {
             Actors = new List<Actor>();
@@ -21,6 +60,7 @@ namespace League_of_Leprechauns
         public Actor()
         {
             Actors.Add(this);
+            spriteEffect = SpriteEffects.None;
         }
 
         public virtual void Update(GameTime gameTime)
@@ -30,7 +70,7 @@ namespace League_of_Leprechauns
 
         public void Draw(SpriteBatch spriteBatch)
         {
-
+            spriteBatch.Draw(texture, Position, null, Color.White, Rotation, Origin, Scale, spriteEffect, Depth);
         }
     }
 }
