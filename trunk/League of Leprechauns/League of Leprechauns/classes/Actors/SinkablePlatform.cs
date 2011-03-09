b089f188b89e9038017f1eb31a1ed871ecce6998
Nico.hvi@gmail.com <Nico.hvi@gmail.com@95fc8098-fdfb-d47c-ab93-935c36e4efd7>
2011-03-09 16:50:07 +0000

    SinkablePlatform.cs - Lagt til, da den tydeligvis ikke var det fra før. LOL ASS - N

---
+++ trunk/League of Leprechauns/League of Leprechauns/classes/Actors/SinkablePlatform.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace LoL.classes.Actors
+{
+    class SinkablePlatform : NonLivingObject
+    {
+        public SinkablePlatform(Vector2 startPosition) : base(startPosition)
+        {
+            this.startPosition = startPosition;
+        }
+
+        private Vector2 startPosition, speed;
+        private int activationTime;
+        private Boolean activated;
+
+        public void activate(GameTime gameTime)
+        {
+            if (activated)
+            {
+                return;
+            }
+            activated = true;
+            activationTime = (int) gameTime.TotalGameTime.TotalMilliseconds;
+            
+        }
+
+        public void deactivate(GameTime gameTime)
+        {
+            if (!activated)
+            {
+                return;
+            }
+            activated = false;
+            activationTime = -1; 
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            if (activated)
+            {
+                startSinking(gameTime);
+            }
+            base.Update(gameTime);
+        }
+
+        private void startSinking(GameTime gameTime)
+        {
+            float accerelation = 1.3f;
+            float elapsedTimeInSeconds = ((int)gameTime.TotalGameTime.TotalMilliseconds - activationTime) / 1000;
+            speed = new Vector2(0, accerelation*elapsedTimeInSeconds*elapsedTimeInSeconds);
+            Position += speed;
+        }
+
+    }
+}
