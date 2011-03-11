11d1b86251e07731b2443f15ef7b354c0874daff
Nico.hvi@gmail.com <Nico.hvi@gmail.com@95fc8098-fdfb-d47c-ab93-935c36e4efd7>
2011-03-11 10:39:47 +0000

    CollisionDetector.cs - Nicolay

--- League of Leprechauns/League of Leprechauns/classes/ActorManager.cs
+++ League of Leprechauns/League of Leprechauns/classes/ActorManager.cs
@@ -4,18 +4,25 @@ using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using LoL.classes;
 
 namespace LoL
 {
     class ActorManager
     {
 
-        public List<Actor> ListOfAllActors;
+        private List<Actor> ListOfAllActors;
+        private List<Actor> ListOfActiveActors;
         private Rectangle actor1Rect, actor2Rect;
+        private CollisionDetector collisionDetector;
+        private CollisionHandler collisionHandler;
 
         public ActorManager()
         {
             ListOfAllActors = new List<Actor>();
+            ListOfActiveActors = new List<Actor>();
+            collisionDetector = new CollisionDetector(this);
+            collisionHandler = new CollisionHandler(this);
         }
 
         public void addActor(Actor actor)
@@ -28,56 +35,26 @@ namespace LoL
             ListOfAllActors.Clear();
         }
 
+        public List<Actor> getListOfActiveActors()
+        {
+            return ListOfActiveActors;
+        }
+
         public List<Actor> getListOfAllActors()
         {
             return ListOfAllActors;
         }
 
+
         public void Update(GameTime gametime)
         {
             /*
-             * Hovedmetoden for kollisjonsdeteksjon.
-             */
-            foreach (Actor actor1 in ListOfAllActors)
+             * Hver aktive actor forsøker å bevege seg,
+             * og kaller CollisionDetector fra sine egne metoder.
+             */ 
+            foreach(Actor actor in ListOfActiveActors)
             {
-                actor1Rect = actor1.generateBoundingRectangle();
-                foreach (Actor actor2 in ListOfAllActors)
-                {
-                    if (actor1 != actor2)
-                    {
-                        actor2Rect = actor2.generateBoundingRectangle();
-                        collisionDetection(actor1Rect, actor2Rect, actor1, actor2);
-                    }
-                }
-            }
-
-        }
-
-        private void collisionDetection(Rectangle actor1Rect, Rectangle actor2Rect, Actor actor1, Actor actor2)
-        {
-            if (actor1Rect.Intersects(actor2Rect))
-            {
-                if (actor1Rect.Bottom >= actor2Rect.Top)
-                {
-                    /* TODO:
-                     * actor1 står på actor2.
-                     */
-                    actor1.collisionUnder(actor2);
-                }
-                else if (actor1Rect.Top >= actor2Rect.Bottom)
-                {
-                    /* TODO:
-                     * actor1 skaller i actor2. 
-                     */
-                    actor1.collisionOver(actor2);
-                }
-                else if (actor1Rect.Right >= actor2Rect.Left || actor1Rect.Left <= actor2Rect.Right)
-                {
-                    /*
-                     * actor1 treffer actor2 fra venstre eller høyre.
-                     */
-                    actor1.collisionSide(actor2);
-                }
+                actor.Update(gametime);
             }
         }
 
