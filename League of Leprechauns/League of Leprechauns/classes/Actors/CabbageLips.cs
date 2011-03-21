b9bd8018f3f6e66873c4507c2965c3427e1ac7b0
kengroenn@gmail.com <kengroenn@gmail.com@95fc8098-fdfb-d47c-ab93-935c36e4efd7>
2011-03-21 11:27:42 +0000

    Nå fungerer PROTO animasjon for CabbageLips.cs.

--- League of Leprechauns/League of Leprechauns/classes/Actors/CabbageLips.cs
+++ League of Leprechauns/League of Leprechauns/classes/Actors/CabbageLips.cs
@@ -14,13 +14,67 @@ namespace LoL
  
         }
 
+        private int timeSinceLastFrame = 0;
+        const int RUNNING_ANIMATION_SPEED = 50;
         public override void Update(GameTime gameTime)
         {
             base.Update(gameTime);
+            timeSinceLastFrame += gameTime.ElapsedGameTime.Milliseconds;
+            if (CurrentSpeed.X < 0)
+            {
+                flipHorizontally(true);
+            }
+            else if (CurrentSpeed.X > 0)
+            {
+                flipHorizontally(false);
+            }
+
+            if (isJumping())
+            {
+                Rectangle jumpFrame = new Rectangle(300, 0, 100, 140);
+                setFrame(jumpFrame);
+            }
+            else if (CurrentSpeed.X != 0)
+            {
+                nextRunningFrame();
+            }
+            else
+            {
+                Rectangle stillFrame = new Rectangle(0, 0, 100, 140);
+                setFrame(stillFrame);
+            }
+        }
+
+        private void nextRunningFrame(){
+            if (timeForNextFrame())
+            {
+                timeSinceLastFrame = 0;
+                Rectangle oldFrame = getFrame();
+                Rectangle nextFrame = new Rectangle((oldFrame.Left + 100) % 300, 0, 100, 140);
+                setFrame(nextFrame);
+            }
+        }
+
+        private void jumpingFrame()
+        {
+            
+        }
+
+        private bool timeForNextFrame()
+        {
+            return timeSinceLastFrame > RUNNING_ANIMATION_SPEED;
         }
 
         public override void move()
         {
+
+        }
+
+        public override void LoadContent(Microsoft.Xna.Framework.Content.ContentManager theContentManager, string theAssetName)
+        {
+            base.LoadContent(theContentManager, theAssetName);
+            Rectangle newFrame = new Rectangle(0,0, 100, 140);
+            setFrame(newFrame);
 
         }
     }
