cd36f7032036ee0f66601ee7bf1af3f3a297a0e6
Nico.hvi@gmail.com <Nico.hvi@gmail.com@95fc8098-fdfb-d47c-ab93-935c36e4efd7>
2011-03-22 15:57:49 +0000

    logikk, blir sikkert noen feil, men fikk sykt mange konflikter når jeg skulle comitte.

--- trunk/League of Leprechauns/League of Leprechauns/classes/Actors/HostileNPC.cs
+++ trunk/League of Leprechauns/League of Leprechauns/classes/Actors/HostileNPC.cs
@@ -3,16 +3,82 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 
 namespace LoL
 {
+    enum FaceDirection
+    {
+        Left = -1,
+        Right = 1
+    }
+    /// <summary>
+    /// Class describing the actions of enemy NPCs.
+    /// </summary>
     class HostileNPC : NonPlayerCharacter
     {
-        public HostileNPC(Vector2 startPosition, int level, Vector2 startSpeed, int totalHealth, Vector2 attackSpeed, int jumpSpeed)
-            : base(startPosition, level, startSpeed, totalHealth, attackSpeed, jumpSpeed) 
-        {
+        private FaceDirection faceDirection;
+        public bool isOnGround;
 
+        public FaceDirection FaceDirection
+        {
+            get { return faceDirection; }
+        }
+
+        public HostileNPC(Vector2 startPosition, int level, int totalHealth, Vector2 attackSpeed, int jumpSpeed)
+            : base(startPosition, level, totalHealth, attackSpeed, jumpSpeed) 
+        {
+            faceDirection = FaceDirection.Left;
+            movementSpeed = Settings.ENEMY_INITIAL_SPEED;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            if (isOnGround)
+            {
+                if (faceDirection > 0)
+                {
+                    base.Move(Direction.RIGHT);
+                }
+                else
+                {
+                    base.Move(Direction.LEFT);
+                }
+            }
+        }
+
+        public void TurnAround()
+        {
+            if (faceDirection > 0)
+            {
+                faceDirection = FaceDirection.Left;
+            }
+            else
+            {
+                faceDirection = FaceDirection.Right;
+            }
+
+            base.AddForce(new Vector2(-PotentialSpeed.X, 0)); // Stopper spriten.
+        }
+
+        public override void HandleCollision(Collision collision)
+        {
+            Vector2 transVector = collision.getTranslationVector();
+            Actor collidingActor = collision.getCollidingActor();
+
+            if (transVector.Y < 0)
+            {
+                isOnGround = true;
+            }
+
+            if (transVector == Vector2.Zero)
+            {
+                if(isOnGround)
+                    TurnAround();
+            }
+
+            base.HandleCollision(collision);
         }
     }
 }
