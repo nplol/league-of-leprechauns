b5cae1808a536cb2b16a28474508f39d79ea2397
kengroenn@gmail.com <kengroenn@gmail.com@95fc8098-fdfb-d47c-ab93-935c36e4efd7>
2011-03-24 12:13:08 +0000

    Added Some Simply Functionality for the AI-Functionality-Framework.

---
+++ League of Leprechauns/League of Leprechauns/classes/Actors/NoAwernessHostileNPC.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace LoL
+{
+    class StupidRollingHostileNPC : HostileNPC
+    {
+        private Direction _movementDirection;
+        private Vector2 startPosition;
+        private List<Actor> actorsDamageAlreadyTaken;
+
+        public StupidRollingHostileNPC(Vector2 startPosition, int level, Vector2 startSpeed, int totalHealth, Direction movementDirection)
+            : base(startPosition, level, totalHealth, 0, 0)
+        {
+            _movementDirection = movementDirection;
+            this.startPosition = startPosition;
+            actorsDamageAlreadyTaken = new List<Actor>();
+        }
+
+        
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+            Move(_movementDirection);
+        }
+
+        public override void HandleCollision(Collision collision)
+        {
+            base.HandleCollision(collision);
+            Vector2 transVec = collision.getTranslationVector();
+            Actor collidingActor = collision.getCollidingActor();
+            if (collidingActor is Character)
+            {
+                collidingActor.AddForce(new Vector2(0, -5*Settings.GRAVITY));
+                if (!actorsDamageAlreadyTaken.Contains(collidingActor))
+                {
+                    ((Character)collidingActor).TakeDamage(-1);
+                    actorsDamageAlreadyTaken.Add(collidingActor);
+                }
+                
+            
+            }
+        
+
+        }
+     
+        private void turnAround()
+        {
+            if (_movementDirection == Direction.LEFT)
+                _movementDirection = Direction.RIGHT;
+            else
+                _movementDirection = Direction.LEFT;
+        }
+
+
+        public override void PerformAbility(AbilityNumber abilityNumber)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
