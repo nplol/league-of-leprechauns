8dd1c5e1e69d8e5b1bb547c620fd05cc7327cbc1
kengroenn@gmail.com <kengroenn@gmail.com@95fc8098-fdfb-d47c-ab93-935c36e4efd7>
2011-03-24 12:13:08 +0000

    Added Some Simply Functionality for the AI-Functionality-Framework.

---
+++ trunk/League of Leprechauns/League of Leprechauns/classes/Actors/AbstractFuzzyOnSightHostileNPC.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace LoL
+{
+    abstract class AbstractFuzzyOnSightHostileNPC : AbstractOnSightHostileNPC
+    {
+        private int _unceraintyConstant;
+        
+        public AbstractFuzzyOnSightHostileNPC(Vector2 startPosition, int level, int totalHealth, int attackSpeed, 
+            int jumpSpeed, int uncertaintyConstant, int sightDistance) 
+            : base(startPosition, level, totalHealth, attackSpeed, jumpSpeed, sightDistance)
+        {
+            _unceraintyConstant = uncertaintyConstant;
+        }
+
+        internal Vector2 GetPossibleAbilityPosition()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal Vector2 GetPossiblePositionOfNearestCharacter()
+        {
+            PlayerCharacter playerCharacter = NearestPlayerCharacter();
+            Vector2 fuzzyPosition = playerCharacter.CurrentPosition + uncertaintyVector(playerCharacter);
+            return fuzzyPosition;
+        }
+
+        private Vector2 uncertaintyVector(PlayerCharacter playerCharacter)
+        {
+            Random aRandom = new Random();
+            int min = Math.Min((int)faceDirection * _unceraintyConstant, 0);
+            int max = Math.Max((int)faceDirection * _unceraintyConstant, 0);
+
+            return new Vector2(aRandom.Next(min, max),
+                aRandom.Next(-1*_unceraintyConstant, _unceraintyConstant));
+        }
+
+    }
+}
---
+++ trunk/League of Leprechauns/League of Leprechauns/classes/Actors/AbstractOnSightHostileNPC.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace LoL
+{
+    abstract class AbstractOnSightHostileNPC : AbstractAwareHostileNPC
+    {
+        private int _sightDistance;
+        private Rectangle _sightRectangle;
+
+         public AbstractOnSightHostileNPC(Vector2 startPosition, int level, int totalHealth, int attackSpeed, 
+            int jumpSpeed, int sightDistance) 
+            : base(startPosition, level, totalHealth, attackSpeed, jumpSpeed)
+        {
+            _sightDistance = sightDistance;
+            _sightRectangle = createSightRectangle();
+        }
+
+         internal abstract void OnInSight();
+
+         public override void Update(GameTime gameTime)
+         {
+             base.Update(gameTime);
+             _sightRectangle = createSightRectangle();
+
+             if (aPlayerCharacterIsInSight())
+                 OnInSight();
+
+         }
+
+         private Boolean aPlayerCharacterIsInSight()
+         {
+             Boolean isInSight = false;
+             foreach (PlayerCharacter playerCharacter in GetObserveablePlayerCharacters())
+             {
+                 isInSight = playerCharacter.BoundingRectangle.Intersects(_sightRectangle);
+                 if (isInSight)
+                     break;
+             }
+             return isInSight;
+         }
+
+         private Rectangle createSightRectangle()
+         {
+             int xPosition = 0;
+             int yPosition = _sightDistance / 2 + (int)CurrentPosition.Y;
+
+             if (faceDirection == Direction.LEFT)
+                 xPosition = (int) CurrentPosition.X - _sightDistance;
+             else
+                 xPosition = (int) CurrentPosition.X;
+
+            
+
+             Rectangle sightRectangle = new Rectangle(xPosition, yPosition, _sightDistance, _sightDistance);
+             return sightRectangle;
+         }
+
+ 
+    }
+}
