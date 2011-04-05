843b44973fe4fb4c2d8b1bae9354df3648b7e4dc
steffen.stenersen@gmail.com <steffen.stenersen@gmail.com@95fc8098-fdfb-d47c-ab93-935c36e4efd7>
2011-04-05 11:31:44 +0000

    Removed unused usings from all files.

--- League of Leprechauns/League of Leprechauns/classes/Level/LevelManager.cs
+++ League of Leprechauns/League of Leprechauns/classes/Level/LevelManager.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 
 using Microsoft.Xna.Framework.Content;
-using System.IO;
 using Microsoft.Xna.Framework.Graphics;
 
 namespace LoL
